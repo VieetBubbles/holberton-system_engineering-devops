e6fa2d35ff0463535ac0c1232e506ac40b7cf14d
VieetBubbles <537@holbertonschool.com>
2019-08-08 17:19:14 +0000

    Added the sed -i command

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,3 +1,3 @@
 # Use Puppet to install Nginx
-exec {'bin/bash/env apt get update apt-get -y install nginx ufw allow Nginx HTTP echo "Holberton School" > /var/www/html/index.nginx-debian.html service nginx restart':
+exec {'bin/bash/env apt get update apt-get -y install nginx ufw allow Nginx HTTP echo "Holberton School" > /var/www/html/index.nginx-debian.html sed -i "/listen 80 default_server/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default service nginx restart':
 }
