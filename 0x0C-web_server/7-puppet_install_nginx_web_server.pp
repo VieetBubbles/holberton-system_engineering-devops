126294e14925375a00bc286b01673cea4f7ff394
VieetBubbles <537@holbertonschool.com>
2019-08-08 17:27:37 +0000

    replaced the bash with usr

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,7 +1,7 @@
 # Use Puppet to install Nginx
-exec {'bin/bash/env apt get update':}
-exec {'bin/bash/env apt-get -y install nginx':}
-exec {'bin/bash/env ufw allow "Nginx HTTP"':}
-exec {'bin/bash/env echo "Holberton School" > /var/www/html/index.nginx-debian.html':}
-exec {'bin/bash/env sed -i "/listen 80 default_server/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default':}
-exec {'bin/bash/env service nginx restart':}
+exec {'/usr/bin/env apt get update':}
+exec {'/usr/bin/env apt-get -y install nginx':}
+exec {'/usr/bin/env ufw allow "Nginx HTTP"':}
+exec {'/usr/bin/env echo "Holberton School" > /var/www/html/index.nginx-debian.html':}
+exec {'/usr/bin/env sed -i "/listen 80 default_server/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default':}
+exec {'/usr/bin/env service nginx restart':}
