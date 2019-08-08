5785df23e1c34239079ead723aa15faf875e71aa
VieetBubbles <537@holbertonschool.com>
2019-08-08 22:32:19 +0000

    Fix previous commit

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Client configuration file (w/ Puppet)
 file_line { 'AddHug':
   ensure => 'present',
-  path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
 }
 
 file_line { 'AddBug':
   ensure => 'present'
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/holberton',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton',
 }
