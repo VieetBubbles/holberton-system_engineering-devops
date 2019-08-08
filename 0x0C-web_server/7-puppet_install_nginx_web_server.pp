b09caa48c13ed10fb841d6899f1ce675228c64cf
VieetBubbles <537@holbertonschool.com>
2019-08-08 17:25:19 +0000

    Used mutiple exec commands

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,3 +1,7 @@
 # Use Puppet to install Nginx
-exec {'bin/bash/env apt get update apt-get -y install nginx ufw allow Nginx HTTP echo "Holberton School" > /var/www/html/index.nginx-debian.html sed -i "/listen 80 default_server/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default service nginx restart':
-}
+exec {'bin/bash/env apt get update':}
+exec {'bin/bash/env apt-get -y install nginx':}
+exec {'bin/bash/env ufw allow "Nginx HTTP"':}
+exec {'bin/bash/env echo "Holberton School" > /var/www/html/index.nginx-debian.html':}
+exec {'bin/bash/env sed -i "/listen 80 default_server/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default':}
+exec {'bin/bash/env service nginx restart':}
